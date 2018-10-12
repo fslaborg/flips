54b5fd65156e6a02d8f6ff26fe42ad36a6e6dab7
matthewcrews <matthewcrews@gmail.com>
2018-10-11 23:17:27 -0400

    Remove ignored files

--- Flips.Console/Program.fs
+++ Flips.Console/Program.fs
@@ -9,7 +9,7 @@ open Flips.Types
 let main argv =
 
 
-    let rhs = Vector.Dense (DenseVector [|4.0; 5.0; 6.0|])
+    let rhs = Vector.Dense (DenseVector [|2.0; 3.0; 4.0|])
     let A = Matrix.Dense (DenseMatrix (array2D [[5.0; 1.0; 2.0]; [3.0; 1.0; 4.0]; [2.0; 6.0; 2.0]]))
     let lu = LinearAlgebra.factorize A
     let x = LinearAlgebra.solve lu rhs
