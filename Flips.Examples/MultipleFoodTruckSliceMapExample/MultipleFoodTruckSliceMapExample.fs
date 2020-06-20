1a87e51cfe2ac4aaeb445d7683e0abeb9dcca087
Matthew <matthewcrews@gmail.com>
2020-06-20 01:51:37 -0400

    Refactored the layout of examples (#51)

---
+++ Flips.Examples/MultipleFoodTruckSliceMapExample/MultipleFoodTruckSliceMapExample.fs
@@ -0,0 +1,86 @@
+﻿module Flips.Examples.MultipleFoodTruckWithSliceMapExample
+
+open Flips.Domain
+open Flips.Solve
+open Flips.SliceMap
+
+let solve () =
+    
+    // Declare the parameters for our model
+    let items = ["Hamburger"; "HotDog"; "Pizza"]
+    let locations = ["Woodstock"; "Sellwood"; "Portland"]
+    let profit = 
+        [
+            (("Woodstock", "Hamburger"), 1.50); (("Sellwood", "Hamburger"), 1.40); (("Portland", "Hamburger"), 1.90)
+            (("Woodstock", "HotDog"   ), 1.20); (("Sellwood", "HotDog"   ), 1.50); (("Portland", "HotDog"   ), 1.80)
+            (("Woodstock", "Pizza"    ), 2.20); (("Sellwood", "Pizza"    ), 1.70); (("Portland", "Pizza"    ), 2.00)
+        ] |> SMap2.ofList
+
+    let maxIngredients = SMap.ofList [("Hamburger", 900.0); ("HotDog", 600.0); ("Pizza", 400.0)]
+    let itemWeight = SMap.ofList [("Hamburger", 0.5); ("HotDog", 0.4); ("Pizza", 0.6)]
+    let maxTruckWeight = SMap.ofList [("Woodstock", 200.0); ("Sellwood", 300.0); ("Portland", 280.0) ]
+
+    // Create Decision Variable which is keyed by the tuple of Item and Location.
+    // The resulting type is a Map<(string*string),Decision> 
+    // to represent how much of each item we should pack for each location
+    // with a Lower Bound of 0.0 and an Upper Bound of Infinity
+    let numberOfItem =
+        [for item in items do
+            for location in locations do
+                let decName = sprintf "NumberOf_%s_At_%s" item location
+                let decision = Decision.createContinuous decName 0.0 infinity
+                (location, item), decision]
+        |> SMap2.ofList
+
+    // Create the Linear Expression for the objective
+    let objectiveExpression = sum (profit .* numberOfItem)
+
+    // Create an Objective with the name "MaximizeRevenue" the goal of Maximizing
+    // the Objective Expression
+    let objective = Objective.create "MaximizeRevenue" Maximize objectiveExpression
+    
+    // Create Total Item Maximum constraints for each item
+    let maxItemConstraints =
+        [for item in items do
+            let name = sprintf "MaxItemTotal|%s" item
+            Constraint.create name (sum (1.0 * numberOfItem.[All, item]) <== maxIngredients.[item])
+        ]
+
+
+    // Create a Constraint for the Max combined weight of items for each Location
+    let maxWeightConstraints = 
+        [for location in locations -> 
+            let name = sprintf "MaxTotalWeight|%s" location
+            Constraint.create name (sum (itemWeight .* numberOfItem.[location, All]) <== maxTruckWeight.[location])
+        ]
+
+    // Create a Model type and pipe it through the addition of the constraints
+    let model =
+        Model.create objective
+        |> Model.addConstraints maxItemConstraints
+        |> Model.addConstraints maxWeightConstraints
+
+    // Create a Settings type which tells the Solver which types of underlying solver to use,
+    // the time alloted for solving, and whether to write an LP file to disk
+    let settings = {
+        SolverType = SolverType.CBC
+        MaxDuration = 10_000L
+        WriteLPFile = None
+    }
+
+    // Call the `solve` function in the Solve module to evaluate the model
+    let result = solve settings model
+
+    printfn "-- Result --"
+
+    // Match the result of the call to solve
+    // If the model could not be solved it will return a `Suboptimal` case with a message as to why
+    // If the model could be solved, it will print the value of the Objective Function and the
+    // values for the Decision Variables
+    match result with
+    | Suboptimal msg -> printfn "Unable to solve. Error: %s" msg
+    | Optimal solution ->
+        printfn "Objective Value: %f" solution.ObjectiveResult
+
+        for (decision, value) in solution.DecisionResults |> Map.toSeq do
+            printfn "Decision: %A\tValue: %f" decision.Name value
---
+++ Flips.Examples/SimpleExample/SimpleExample.fs
@@ -0,0 +1,26 @@
+﻿module Flips.Examples.SimpleExample
+
+open Flips.Domain
+open Flips.Solve
+
+let solve () =
+    let x1 = Decision.createContinuous "x1" 0.0 infinity
+    let x2 = Decision.createContinuous "x2" 0.0 infinity
+    
+    let objExpr = 2.0 * x1 + 3.0 * x2
+    let objective = Objective.create "Get big" Maximize objExpr
+    let model = 
+        Model.create objective
+        |> Model.addConstraint (Constraint.create "Max x1" (x1 <== 10.0))
+        |> Model.addConstraint (Constraint.create "Max x2" (x2 <== 5.0))
+        |> Model.addConstraint (Constraint.create "Max x1 and x2" (x1 + x2 <== 12.0))
+    
+    let settings = {
+        SolverType = SolverType.CBC
+        MaxDuration = 30_000L
+        WriteLPFile = Some "Test.lp"
+    }
+    
+    let result = solve settings model
+    printfn "%A" result
+
