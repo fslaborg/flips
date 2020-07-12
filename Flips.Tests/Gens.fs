b8076fd3c7ff8593c842f8141462e961a6f99bb7
Matthew Crews <matthewcrews@gmail.com>
2020-07-12 00:25:34 -0700

    Cleaning up tests

--- Flips.Tests/Gens.fs
+++ Flips.Tests/Gens.fs
@@ -1,167 +1,258 @@
-﻿module Flips.Gens
+﻿namespace Flips.Tests
 
-open Flips.Types
-open FsCheck
+module Gens =
 
-type SmallFloat = SmallFloat of float
+    open Flips
+    open Flips.Types
+    open FsCheck
 
-let MIN_FLOAT = -1e18
-let MAX_FLOAT = 1e18
-let MIN_DISTANCE_FROM_ZERO = 1e-9
+    type SmallFloat = SmallFloat of float
 
-let FloatGen = Arb.generate<float>.Where(fun x -> x > MIN_FLOAT && x < MAX_FLOAT && (x > (MIN_DISTANCE_FROM_ZERO) || x < (-1.0 * MIN_DISTANCE_FROM_ZERO)))
+    let MIN_FLOAT = -1e18
+    let MAX_FLOAT = 1e18
+    let MIN_DISTANCE_FROM_ZERO = 1e-9
 
-let PositiveFloatGen = Arb.generate<float>.Where(fun x -> x > 0.0 && x < MAX_FLOAT)
+    let FloatGen = Arb.generate<float>.Where(fun x -> x > MIN_FLOAT && x < MAX_FLOAT && (x > (MIN_DISTANCE_FROM_ZERO) || x < (-1.0 * MIN_DISTANCE_FROM_ZERO)))
 
-let SmallFloatGen =
-    gen {
-        let! f = FloatGen
-        return SmallFloat f
-    }
+    let PositiveFloatGen = Arb.generate<float>.Where(fun x -> x > 0.0 && x < MAX_FLOAT)
 
-let ScalarGen =
-    gen {
-        let! f = FloatGen
-        return Value f
-    }
+    let SmallFloatGen =
+        gen {
+            let! f = FloatGen
+            return SmallFloat f
+        }
 
-let IntegerBoundsGen =
-    gen {
-        let! lb = FloatGen
-        let! d = PositiveFloatGen
-        let lowerBound = lb
-        let delta = d
-        let upperBound = lowerBound + delta
-        return Integer (lowerBound, upperBound)
-    }
+    let ScalarGen =
+        gen {
+            let! f = FloatGen
+            return Value f
+        }
+
+    let IntegerBoundsGen =
+        gen {
+            let! lb = FloatGen
+            let! d = PositiveFloatGen
+            let lowerBound = lb
+            let delta = d
+            let upperBound = lowerBound + delta
+            return Integer (lowerBound, upperBound)
+        }
     
-let ContinuousBoundsGen =
-    gen {
-        let! lb = FloatGen
-        let! d = PositiveFloatGen
-        let lowerBound = lb
-        let delta = d
-        let upperBound = lowerBound + delta
-        return Continuous (lowerBound, upperBound)
-    }
-
-let DecisionTypeGen =
-    gen {
-        let! integerBounds = IntegerBoundsGen
-        let! continuousBounds = ContinuousBoundsGen
-        return! Gen.elements [integerBounds; continuousBounds; Boolean]
-    }
-
-let DecisionNameGen =
-    gen {
-        let! (NonEmptyString name) = Arb.generate<NonEmptyString>
-        return DecisionName name
-    }
-
-let DecisionGen = 
-    gen {
-        let! name = DecisionNameGen
-        let! decisionType = DecisionTypeGen
-        let d = {
-            Name = name
-            Type = decisionType
+    let ContinuousBoundsGen =
+        gen {
+            let! lb = FloatGen
+            let! d = PositiveFloatGen
+            let lowerBound = lb
+            let delta = d
+            let upperBound = lowerBound + delta
+            return Continuous (lowerBound, upperBound)
         }
-        return d
-    }
 
-let DecisionExpressionGen =
-    gen {
-        let! coefficient = Arb.generate<float>
-        let! decision = Arb.generate<Decision>
-        return coefficient * decision
-    }
-
-let LinearExpressionGen =
-    gen {
-        let! offsetValue = Arb.generate<float>
-        let! offset = Gen.elements [None; Some offsetValue]
-        let! decisionElements = DecisionExpressionGen.ListOf 100
-        let decisionExpr = decisionElements |> Seq.sum
-        let finalExpr = match offset with | Some x -> decisionExpr + x | None -> decisionExpr
-        return finalExpr
-    }
-
-let InequalityGen = 
-    gen {
-        return! Gen.elements [LessOrEqual; GreaterOrEqual]
-    }
-
-let ConstraintExpressionGen = 
-    gen {
-        let! lhs = LinearExpressionGen
-        let! rhs = LinearExpressionGen
-        let! inequality = InequalityGen
-        return! Gen.elements [Inequality (lhs, inequality, rhs); Equality (lhs, rhs)]
-    }
-
-let ConstraintNameGen = 
-    gen {
-        let! NonEmptyString name = Arb.generate<NonEmptyString>
-        return ConstraintName name
-    }
-
-let ConstraintGen =
-    gen {
-        let! name = ConstraintNameGen
-        let! constraintExpression = ConstraintExpressionGen
-        let cnst = {
-            Name = name
-            Expression = constraintExpression
+    let DecisionTypeGen =
+        gen {
+            let! integerBounds = IntegerBoundsGen
+            let! continuousBounds = ContinuousBoundsGen
+            return! Gen.elements [integerBounds; continuousBounds; Boolean]
         }
-        return cnst
-    }
 
-let ObjectiveSenseGen =
-    gen {
-        return! Gen.elements [Minimize; Maximize]
-    }
-
-let ObjectiveNameGen =
-    gen {
-        let! NonEmptyString name = Arb.generate<NonEmptyString>
-        return ObjectiveName name
-    }
-
-let ObjectiveGen =
-    gen {
-        let! name = ObjectiveNameGen
-        let! sense = ObjectiveSenseGen
-        let! expr = LinearExpressionGen
-        let objective = {
-            Name = name
-            Sense = sense
-            Expression = expr
+    let DecisionNameGen =
+        gen {
+            let! (NonEmptyString name) = Arb.generate<NonEmptyString>
+            return DecisionName name
         }
-        return objective
-    }
 
-let ModelGen =
-    gen {
-        let! objective = ObjectiveGen
-        let! constraints = ConstraintGen.ListOf 100
-        let model =
-            Model.create objective
-            |> Model.addConstraints constraints
-        return model
-    }
+    let DecisionGen = 
+        gen {
+            let! name = DecisionNameGen
+            let! decisionType = DecisionTypeGen
+            let d = {
+                Name = name
+                Type = decisionType
+            }
+            return d
+        }
 
-type Domain () =
-    static member ArbSmallFloatGen () = Arb.fromGen SmallFloatGen
-    static member ArbScalarGen () = Arb.fromGen ScalarGen
-    static member ArbDecisionTypeGen () = Arb.fromGen DecisionTypeGen
-    static member ArbDecisionNameGen () = Arb.fromGen DecisionNameGen
-    static member ArbDecision () = Arb.fromGen DecisionGen
-    static member ArbLinearExpression () = Arb.fromGen LinearExpressionGen
-    static member ArbInequality () = Arb.fromGen InequalityGen
-    static member ArbConstraintName () = Arb.fromGen ConstraintNameGen
-    static member ArbConstraintExpression () = Arb.fromGen ConstraintExpressionGen
-    static member ArbConstraint () = Arb.fromGen ConstraintGen
-    static member ArbObjectiveSense () = Arb.fromGen ObjectiveSenseGen
-    static member ArbObjectiveName () = Arb.fromGen ObjectiveNameGen
-    static member ArbObjective () = Arb.fromGen ObjectiveGen
-    static member ArbModel () = Arb.fromGen ModelGen
+    let DecisionExpressionGen =
+        gen {
+            let! coefficient = Arb.generate<float>
+            let! decision = Arb.generate<Decision>
+            return coefficient * decision
+        }
+
+    let LinearExpressionGen =
+        gen {
+            let! offsetValue = Arb.generate<float>
+            let! offset = Gen.elements [None; Some offsetValue]
+            let! decisionElements = DecisionExpressionGen.ListOf 100
+            let decisionExpr = decisionElements |> Seq.sum
+            let finalExpr = match offset with | Some x -> decisionExpr + x | None -> decisionExpr
+            return finalExpr
+        }
+
+    let InequalityGen = 
+        gen {
+            return! Gen.elements [LessOrEqual; GreaterOrEqual]
+        }
+
+    let ConstraintExpressionGen = 
+        gen {
+            let! lhs = LinearExpressionGen
+            let! rhs = LinearExpressionGen
+            let! inequality = InequalityGen
+            return! Gen.elements [Inequality (lhs, inequality, rhs); Equality (lhs, rhs)]
+        }
+
+    let ConstraintNameGen = 
+        gen {
+            let! NonEmptyString name = Arb.generate<NonEmptyString>
+            return ConstraintName name
+        }
+
+    let ConstraintGen =
+        gen {
+            let! name = ConstraintNameGen
+            let! constraintExpression = ConstraintExpressionGen
+            let cnst = {
+                Name = name
+                Expression = constraintExpression
+            }
+            return cnst
+        }
+
+    let ObjectiveSenseGen =
+        gen {
+            return! Gen.elements [Minimize; Maximize]
+        }
+
+    let ObjectiveNameGen =
+        gen {
+            let! NonEmptyString name = Arb.generate<NonEmptyString>
+            return ObjectiveName name
+        }
+
+    let ObjectiveGen =
+        gen {
+            let! name = ObjectiveNameGen
+            let! sense = ObjectiveSenseGen
+            let! expr = LinearExpressionGen
+            let objective = {
+                Name = name
+                Sense = sense
+                Expression = expr
+            }
+            return objective
+        }
+
+    let ModelGen =
+        gen {
+            let! objective = ObjectiveGen
+            let! constraints = ConstraintGen.ListOf 100
+            let model =
+                Model.create objective
+                |> Model.addConstraints constraints
+            return model
+        }
+
+    type Types () =
+        static member ArbSmallFloatGen () = Arb.fromGen SmallFloatGen
+        static member ArbScalarGen () = Arb.fromGen ScalarGen
+        static member ArbDecisionTypeGen () = Arb.fromGen DecisionTypeGen
+        static member ArbDecisionNameGen () = Arb.fromGen DecisionNameGen
+        static member ArbDecision () = Arb.fromGen DecisionGen
+        static member ArbLinearExpression () = Arb.fromGen LinearExpressionGen
+        static member ArbInequality () = Arb.fromGen InequalityGen
+        static member ArbConstraintName () = Arb.fromGen ConstraintNameGen
+        static member ArbConstraintExpression () = Arb.fromGen ConstraintExpressionGen
+        static member ArbConstraint () = Arb.fromGen ConstraintGen
+        static member ArbObjectiveSense () = Arb.fromGen ObjectiveSenseGen
+        static member ArbObjectiveName () = Arb.fromGen ObjectiveNameGen
+        static member ArbObjective () = Arb.fromGen ObjectiveGen
+        static member ArbModel () = Arb.fromGen ModelGen
+
+    module UnitsOfMeasure =
+        
+        open Flips.UnitsOfMeasure
+        open Flips.UnitsOfMeasure.Types
+
+        type [<Measure>] Item
+        type SmallFloatItem = SmallFloat of float<Item>
+
+        let MIN_FLOAT_ITEM = MIN_FLOAT * 1.0<Item>
+        let MAX_FLOAT_ITEM = MAX_FLOAT * 1.0<Item>
+        let MIN_DISTANCE_FROM_ZERO_ITEM = MIN_DISTANCE_FROM_ZERO * 1.0<Item>
+
+        let FloatItemGen = Arb.generate<float<Item>>.Where(fun x -> x > MIN_FLOAT_ITEM && x < MAX_FLOAT_ITEM && (x > (MIN_DISTANCE_FROM_ZERO_ITEM) || x < (-1.0 * MIN_DISTANCE_FROM_ZERO_ITEM)))
+
+        let PositiveFloatItemGen = Arb.generate<float<Item>>.Where(fun x -> x > 0.0<Item> && x < MAX_FLOAT_ITEM)
+
+        let SmallFloatItemGen =
+            gen {
+                let! f = FloatItemGen
+                return SmallFloat f
+            }
+
+        //let IntegerBoundsGen =
+        //    gen {
+        //        let! lb = FloatGen
+        //        let! d = PositiveFloatGen
+        //        let lowerBound = lb
+        //        let delta = d
+        //        let upperBound = lowerBound + delta
+        //        return Integer (lowerBound, upperBound)
+        //    }
+         
+        //let ContinuousBoundsGen =
+        //    gen {
+        //        let! lb = FloatGen
+        //        let! d = PositiveFloatGen
+        //        let lowerBound = lb
+        //        let delta = d
+        //        let upperBound = lowerBound + delta
+        //        return Continuous (lowerBound, upperBound)
+        //    }
+
+        //let DecisionTypeGen =
+        //    gen {
+        //        let! integerBounds = IntegerBoundsGen
+        //        let! continuousBounds = ContinuousBoundsGen
+        //        return! Gen.elements [integerBounds; continuousBounds; Boolean]
+        //    }
+
+        //let DecisionNameGen =
+        //    gen {
+        //        let! (NonEmptyString name) = Arb.generate<NonEmptyString>
+        //        return DecisionName name
+        //    }
+
+        let DecisionItemGen = 
+            gen {
+                let! name = DecisionNameGen
+                let! decisionType = DecisionTypeGen
+                let d = {
+                    Name = name
+                    Type = decisionType
+                }
+                return Decision<Item>.Value d
+            }
+
+        let DecisionItemExpressionGen =
+            gen {
+                let! coefficient = Arb.generate<float>
+                let! decision = Arb.generate<Decision<Item>>
+                return coefficient * decision
+            }
+
+        let LinearExpressionItemGen =
+            gen {
+                let! offsetValue = Arb.generate<float>
+                let! offset = Gen.elements [None; Some offsetValue]
+                let! decisionElements = DecisionExpressionGen.ListOf 100
+                let decisionExpr = decisionElements |> Seq.sum
+                let finalExpr = match offset with | Some x -> decisionExpr + x | None -> decisionExpr
+                return LinearExpression<Item>.Value finalExpr
+            }
+
+        type UnitOfMeasureTypes =
+            static member ArbDecisionItem () = Arb.fromGen DecisionItemGen
+            static member ArbLinearExpressionItem () = Arb.fromGen LinearExpressionItemGen
+            
