d87312847c651010223de75d20955a302a4b98a5
isaacabraham <isaac.abraham@gmail.com>
2020-11-13 23:35:09 +0100

    Fix up malform return xml

--- Flips/ISliceData.fs
+++ Flips/ISliceData.fs
@@ -24,15 +24,15 @@ module Sum =
 
     /// <summary>A function which sums the values contained in a SliceMap</summary>
     /// <param name="x">An instance of ISliceData</param>
-    /// <returns>A LinearExpression</returns
+    /// <returns>A LinearExpression</returns>
     let inline sum (x:ISliceData<'Key, 'Value>) : Flips.Types.LinearExpression =
         TryFind.sum x.Keys x.TryFind
 
     /// <summary>A function which sums a sequence of SliceMaps</summary>
     /// <param name="x">A sequence of SliceMaps</param>
-    /// <returns>A LinearExpression</returns
-    let inline sumAll< ^a, ^b when ^a: (static member Sum: ^a -> Flips.Types.LinearExpression) 
+    /// <returns>A LinearExpression</returns>
+    let inline sumAll< ^a, ^b when ^a: (static member Sum: ^a -> Flips.Types.LinearExpression)
                               and ^a: (static member (+): ^a * ^a -> ^a)
-                              and ^a: (static member Zero: ^a)> (k1: ^a seq) : Flips.Types.LinearExpression = 
+                              and ^a: (static member Zero: ^a)> (k1: ^a seq) : Flips.Types.LinearExpression =
         let r = Seq.sum k1
         ((^a) : (static member Sum: ^a -> Flips.Types.LinearExpression) r)
