27326d16e1c71485dcc1b90a7a4ceb29c4ea9136
Matthew Crews <matthewcrews@gmail.com>
2020-11-23 23:42:41 -0800

    making progress but aliasis not working

--- Flips/Types.fs
+++ Flips/Types.fs
@@ -14,7 +14,6 @@ type internal SignInsenstiveComparer () =
 /// An Integer type can take on any discrete value between the Upper and Lower Bound (inclusive)
 /// A Continuous type can take on any value between the Upper and Lower bound (inclusive)
 type DecisionType =
-    internal
     | Boolean
     | Integer of LowerBound: float * UpperBound: float
     | Continuous of LowerBound: float * UpperBound: float
@@ -32,11 +31,10 @@ type internal ReduceAccumulator = {
 }
 
 /// Represents a decision that must be made
-type Decision = 
-    internal {
-        Name : DecisionName
-        Type : DecisionType
-    }
+type Decision =  {
+    Name : DecisionName
+    Type : DecisionType
+}
 with
 
     static member (*) (decision: Decision, f: float) =
