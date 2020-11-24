7d5cac1d8ed3eb064f1b74d6ef685e6b11c8c133
Gauthier Segay <gauthier.segay@gmail.com>
2020-11-24 02:57:10 -0800

    lots of adjustments
    * Flips.Solver.FlipsVersion2 project which will hold the current API
    * split the files a bit in Flips.Solver.FlipsVersion2
    * skeleton Flips.Solver.Cplex project
    * adjust paket.dependencies to have several groups
    * deal with internals visible to in rudimentary manner for now
    
    note: tests and examples are not building

---
+++ Flips/AssemblyInfo.InternalsVisibleTo.fs
@@ -0,0 +1,12 @@
+﻿module AssemblyInfo.InternalsVisibleTo
+
+open System.Runtime.CompilerServices
+
+[<assembly: InternalsVisibleTo("Flips.Solver")>]
+[<assembly: InternalsVisibleTo("Flips.Solver.CBC")>]
+[<assembly: InternalsVisibleTo("Flips.Solver.Cplex")>]
+[<assembly: InternalsVisibleTo("Flips.Solver.FlipsVersion2")>]
+[<assembly: InternalsVisibleTo("Flips.Solver.OPTANO")>]
+
+do()
+
