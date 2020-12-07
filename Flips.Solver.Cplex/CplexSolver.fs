c71fcd6090bc6dc83ced52070bc89a993194e4b5
Gauthier Segay <smoothdeveloper@users.noreply.github.com>
2020-12-06 21:33:33 -0800

    few additions to cplex solver to cover their options and callbacks APIs (#128)
    * Flips.Solver.Cplex: settings & callbacks
    
    * set cplex settings value
    * handling of cplex callback through cplex.Use (https://www-eio.upc.edu/lceio/manuals/cplex-11/html/refdotnetcplex/html/ILOG.CPLEX.Cplex.Use_overloads.html)
    
    also fix stack overflow in own type extensions of Flips.Types.*.GetDecisions() by calling to `LinearExpression.Reduce`
    
    * better honouring settings on obfuscating names or not when converting to cplex
    pretty printer for some types of Flips.Types

--- Flips.Solver.Cplex/CplexSolver.fs
+++ Flips.Solver.Cplex/CplexSolver.fs
@@ -10,8 +10,10 @@ open ILOG.CPLEX
 open ILOG.Concert
 
 module rec CplexSolver =
+    open System
         
     module Primitives =
+
         type Vars = IReadOnlyDictionary<DecisionName, INumVar>
 
         let setVariable  ({Type = decisionType; Name = DecisionName name }) (var:INumVar) =
@@ -53,25 +55,36 @@ module rec CplexSolver =
 
         let addObjective (vars: Vars) (objective: Flips.Types.Objective) state =
             let expr = buildExpression vars objective.Expression state
-            let (ObjectiveName(name)) = objective.Name
+            
             match state.cplex.GetObjective() with
-            | null -> state.cplex.AddObjective(objective.Sense.ToCplex, expr, name)
+            | null ->
+              let obfuscateNames = state.toCplexContext.obfuscateVarNames
+              if obfuscateNames then
+                state.cplex.AddObjective(objective.Sense.ToCplex, expr)
+              else
+                let (ObjectiveName(name)) = objective.Name
+                state.cplex.AddObjective(objective.Sense.ToCplex, expr, name)
             | cplexObjective ->
                 if state.options.HasDoNotReuseState then
                     failwithf "shouldn't reuse cplex state"
                 else
                     cplexObjective.Expr <- expr
-                    cplexObjective.Name <- name
+                    let obfuscateNames = state.toCplexContext.obfuscateVarNames
+                    if not obfuscateNames then
+                      let (ObjectiveName(name)) = objective.Name
+                      cplexObjective.Name <- name
                     cplexObjective.Sense <- objective.Sense.ToCplex
                     cplexObjective
 
         let addConstraint (vars: Vars) (c: Flips.Types.Constraint) state =
+            
             let lhs = buildExpression vars c.Expression.LHS state
             let rhs = buildExpression vars c.Expression.RHS state
+            let obfuscateNames = state.toCplexContext.obfuscateConstraintNames 
             match c.Expression with
-            | Equality (_, _)                  -> state.cplex.AddEq(lhs, rhs)
-            | Inequality(_, GreaterOrEqual, _) -> state.cplex.AddGe(lhs, rhs)
-            | Inequality(_, LessOrEqual, _)    -> state.cplex.AddLe(lhs, rhs)
+            | Equality (_, _)                  -> if obfuscateNames then state.cplex.AddEq(lhs, rhs) else state.cplex.AddEq(lhs, rhs, c.Name.AsString)
+            | Inequality(_, GreaterOrEqual, _) -> if obfuscateNames then state.cplex.AddGe(lhs, rhs) else state.cplex.AddGe(lhs, rhs, c.Name.AsString)
+            | Inequality(_, LessOrEqual, _)    -> if obfuscateNames then state.cplex.AddLe(lhs, rhs) else state.cplex.AddLe(lhs, rhs, c.Name.AsString)
         
         let addConstraints vars constraints state =
             [
@@ -89,80 +102,148 @@ module rec CplexSolver =
                         if state.ofCplexContext.unknownDecisionTurnsToZero then
                             d, 0.
                         else
-                            raise (Exception(sprintf "Decision %A was somehow not setup" d.Name))
+                            raise (System.Exception(sprintf "Decision %A was somehow not setup" d.Name))
                 )
                 |> Map.ofSeq
             {|
                 DecisionResults = decisionMap
-                ObjectiveResult = Flips.Types.LinearExpression.Evaluate decisionMap objective.Expression
+                ObjectiveValue = Flips.Types.LinearExpression.Evaluate decisionMap objective.Expression
             |}
 
-        let solve (model: Flips.Model.Model) state =
-            let decisionsByName = 
-                [
-                    for c in model.Constraints do
-                        yield! c.Expression.GetDecisions()
-                    for o in model.Objectives do
-                        yield! o.Expression.GetDecisions()
-                ]
-                |> HashSet
-                |> Seq.map (fun d -> d.Name, d)
-                |> readOnlyDict
-            
-            let vars =
-                match state.problem.flipsDecisionToCplexNumVar with
-                | :? Dictionary<_,_> as d -> d
-                | :? IReadOnlyDictionary<_,_> as d ->
-                    let d =
-                        [for (KeyValue(k,v)) in d -> k,v]
-                        |> dict 
-                        |> Dictionary
-                    state.problem <- {state.problem with flipsDecisionToCplexNumVar = d }
+        let runSolverWithCallbacks (model: Flips.Types.Model) state (callbackHandler: Cplex.Callback array) =
+          state.cplex.ClearCallbacks()
+          if not (isNull callbackHandler) then
+            for callback in callbackHandler do
+              if not (isNull callback) then
+                state.cplex.Use callback
+          let decisionsByName = 
+              [
+                  for d in model.GetDecisions() do
                     d
-                | _ ->
-                    let d = Dictionary<_,_>()
-                    state.problem <- {state.problem with flipsDecisionToCplexNumVar = d }
-                    d
+              ]
+              |> HashSet
+              |> Seq.map (fun d -> d.Name, d)
+              |> readOnlyDict
 
-            for KeyValue(name,decision) in decisionsByName do
+          let vars =
+              match state.problem.flipsDecisionToCplexNumVar with
+              | :? Dictionary<_,_> as d -> d
+              | :? IReadOnlyDictionary<_,_> as d ->
+                  let d =
+                      [for (KeyValue(k,v)) in d -> k,v]
+                      |> dict 
+                      |> Dictionary
+                  state.problem <- {state.problem with flipsDecisionToCplexNumVar = d }
+                  d
+              | _ ->
+                  let d = Dictionary<_,_>(decisionsByName.Count)
+                  state.problem <- {state.problem with flipsDecisionToCplexNumVar = d }
+                  d
 
-                match vars.TryGetValue name with
-                | false, _ ->
-                    vars.[name] <- 
-                        let cplexVar = createVariable decision state
-                        state.cplex.Add cplexVar |> ignore
-                        
-                        cplexVar
-                | true, var ->
-                    
-                    if state.options.HasDoNotReuseState then
-                        failwithf "using the same solver more than once is not an option"
-                    else
-                        printfn "variable %A already exists, removing before re-adding" name
-                        setVariable decision var
-                
-            let cx = addConstraints vars model.Constraints state
-            let objective = model.Objectives.Head
-            let _ = addObjective vars objective state
+          for KeyValue(name,decision) in decisionsByName do
+              match vars.TryGetValue name with
+              | false, _ ->
+                  vars.[name] <- 
+                      let cplexVar = createVariable decision state
+                      state.cplex.Add cplexVar |> ignore
+                      cplexVar
+              | true, var ->
+                  if state.options.HasDoNotReuseState then
+                      failwithf "using the same solver more than once is not an option"
+                  else
+                      printfn "variable %A already exists, removing before re-adding" name
+                      setVariable decision var
 
-            // look for WriteToFile option
-            state.options.ExportToFiles 
-            |> Seq.iter(state.cplex.ExportModel)
-            
-            if state.cplex.Solve() then
-                let solution = buildSolution decisionsByName.Values vars state objective
-                Ok solution
-            else
-                Error ""
+          let cx = addConstraints vars model.Constraints state
+          let objective = model.Objectives.Head
 
+          let _ = addObjective vars objective state
 
+          // look for WriteToFile option
+          state.options.ExportToFiles 
+          |> Seq.iter(state.cplex.ExportModel)
+
+          use __ = 
+            { new IDisposable with 
+              member x.Dispose () =
+                state.cplex.ClearCallbacks()
+            } 
+
+          if state.cplex.Solve() then
+              let solution = buildSolution decisionsByName.Values vars state objective
+              Ok solution
+          else
+              Error ""
+
+          
+        let runSolverWithCallback (model: Flips.Types.Model) state (callbackHandler: Cplex.Callback) =
+            runSolverWithCallbacks model state [|callbackHandler|]
+
+        let solve (model: Flips.Types.Model) state = runSolverWithCallback model state null
+  
     type ICPlexOption =
         inherit System.IComparable
-        
+
+    [<CustomEquality;CustomComparison>]
+    type NativeParam = 
+      | NativeParam of param: ILOG.CPLEX.Cplex.Param
+      override x.GetHashCode () =
+        let (NativeParam p) = x 
+        p.GetType().Name.GetHashCode() ^^^ p.GetValue()
+      override x.Equals other =
+        (x :> IComparable).CompareTo(other) = 0
+      interface IComparable with
+        member x.CompareTo other =
+          if isNull other then
+            1
+          else
+            match other with
+            | :? NativeParam as other ->
+              match other with
+              | NativeParam other -> 
+                let (NativeParam x) = x
+                let nameCompare = x.GetType().Name.CompareTo (other.GetType().Name)
+                if nameCompare = 0 then
+                  x.GetValue().CompareTo( other.GetValue())
+                else
+                  nameCompare
+            | _ -> 1
+          
     type CplexOption =
         | DoNotReuseState
         | WriteToFile of fileName: string
-        interface ICPlexOption
+        | CplexIntOption of Cplex.IntParam * value: int
+        | CplexLongOption of Cplex.LongParam * value: int64
+        | CplexBoolOption of Cplex.BooleanParam * value: bool
+        | CplexDoubleOption of Cplex.DoubleParam * value: float
+        | CplexStringOption of Cplex.StringParam * value: string
+        
+        member x.CompareTo (other: obj) = 
+          if isNull other then 1 else
+          match other with
+          | :? CplexOption as other ->
+            match other, x with
+            | CplexIntOption(p,v), CplexIntOption(p2,v2) ->
+              if p = p2 then v2.CompareTo v
+              else p2.GetType().Name.CompareTo (p.GetType().Name)
+            | CplexBoolOption(p,v), CplexBoolOption(p2,v2) ->
+              if p = p2 then v2.CompareTo v
+              else p2.GetType().Name.CompareTo (p.GetType().Name)
+            | CplexDoubleOption(p,v), CplexDoubleOption(p2,v2) ->
+              if p = p2 then v2.CompareTo v
+              else p2.GetType().Name.CompareTo (p.GetType().Name)
+            | CplexStringOption(p,v), CplexStringOption(p2,v2) ->
+              if p = p2 then v2.CompareTo v
+              else p2.GetType().Name.CompareTo (p.GetType().Name)
+            | CplexLongOption(p,v), CplexLongOption(p2,v2) ->
+              if p = p2 then v2.CompareTo v
+              else p2.GetType().Name.CompareTo (p.GetType().Name)
+            | _ -> 1
+          | _ -> 1
+
+        interface ICPlexOption with
+          member x.CompareTo other =  x.CompareTo other
+
 
     type CplexOptions =
         {
@@ -176,26 +257,98 @@ module rec CplexSolver =
             |> Seq.choose (function WriteToFile file -> Some file | _ -> None)
             |> Seq.toArray
 
+    type ICplexRunningSolverState = 
+      internal {
+        solverState: ICplexSolverState
+        task: System.Threading.Tasks.Task
+      }
+
+    type State = 
+      static member ofCplex (cplex: Cplex) = ICplexSolverState.createWithCplex cplex
+      static member getCplex state         = state.cplex
+
     type ICplexSolverState =
         internal {
             cplex: Cplex
             mutable problem: CPlexProblemState
             lock: obj
             ofCplexContext: OfCplexContext
+            toCplexContext: ToCplexContext
             options       : CplexOptions
         }
 
-        static member create () =
-            { cplex          = new Cplex()
-              problem        = createEmpty()
-              lock           = obj()
-              ofCplexContext = { unknownDecisionTurnsToZero = true }
-              options        = {options =  Set.ofList [DoNotReuseState] }
-            }
+        member x.TryGetNumVar decisionName = x.problem.flipsDecisionToCplexNumVar.TryGetValue decisionName
+        member x.TryGetConstraint constraintName = x.problem.flipsConstraintToCplexExpr.TryGetValue constraintName
+        
+        static member createWithCplex cplex =
+          let event = new Event<_>()
+          {
+            cplex          = cplex
+            problem        = createEmpty()
+            lock           = obj()
+            ofCplexContext = { unknownDecisionTurnsToZero = true }
+            toCplexContext = { obfuscateConstraintNames = false; obfuscateVarNames = false }
+            options        = { options =  Set.ofList [DoNotReuseState] }
+          }
+        
+        static member create () = ICplexSolverState.createWithCplex(new Cplex())
+              
 
         static member setOption cplexOption (state: ICplexSolverState) = 
             { state with options = { state.options with options = Set.add cplexOption state.options.options } }
 
+        static member setCplexStringOption (value: string) (param: ILOG.CPLEX.Cplex.StringParam) (state: ICplexSolverState) = 
+            state.cplex.SetParam(param, value)
+            let option : ICPlexOption = CplexStringOption(param, value) :> _
+            ICplexSolverState.setOption option state
+        static member setCplexIntOption (param: ILOG.CPLEX.Cplex.IntParam) (value: int)  (state: ICplexSolverState) = 
+            state.cplex.SetParam(param, value)
+            let option : ICPlexOption = CplexIntOption(param, value) :> _
+            ICplexSolverState.setOption option state
+        static member setCplexLongOption (param: ILOG.CPLEX.Cplex.LongParam) (value: int64) (state: ICplexSolverState) = 
+            state.cplex.SetParam(param, value)
+            let option : ICPlexOption = CplexLongOption(param, value) :> _
+            ICplexSolverState.setOption option state
+        static member setCplexDoubleOption (param: ILOG.CPLEX.Cplex.DoubleParam) (value: float) (state: ICplexSolverState) = 
+            state.cplex.SetParam(param, value)
+            let option : ICPlexOption = CplexDoubleOption(param, value) :> _
+            ICplexSolverState.setOption option state
+        static member setCplexBoolOption (param: ILOG.CPLEX.Cplex.BooleanParam) (value: bool) (state: ICplexSolverState) = 
+            state.cplex.SetParam(param, value)
+            let option : ICPlexOption = CplexBoolOption(param, value) :> _
+            
+            ICplexSolverState.setOption option state
+
+        /// remark: use removeCplexOptions if many
+        static member removeCplexOption (o: ILOG.CPLEX.Cplex.Param) state =
+          let otherValue = o.GetValue()
+          let remaining = 
+            [|
+              for p in state.cplex.GetParameterSet() do
+                match p with
+                | :? ILOG.CPLEX.Cplex.Param as p ->
+                  if p.GetValue () = otherValue then
+                    ()
+                | p ->
+                  match p with
+                  | :? Cplex.IntParam as p     -> if p.GetValue() = otherValue then () else p :> Cplex.Param, box (state.cplex.GetParam(p))
+                  | :? Cplex.LongParam as p    -> if p.GetValue() = otherValue then () else p :> Cplex.Param, box (state.cplex.GetParam(p))
+                  | :? Cplex.StringParam as p  -> if p.GetValue() = otherValue then () else p :> Cplex.Param, box (state.cplex.GetParam(p))
+                  | :? Cplex.BooleanParam as p -> if p.GetValue() = otherValue then () else p :> Cplex.Param, box (state.cplex.GetParam(p))
+                  | :? Cplex.DoubleParam as p  -> if p.GetValue() = otherValue then () else p :> Cplex.Param, box (state.cplex.GetParam(p))
+                  | _ -> failwith $"can't get param value %A{p}"
+                  ()
+            |]
+          state.cplex.GetParameterSet().Clear()
+          for p,v in remaining do
+            match p with
+            | :? Cplex.IntParam as p     -> state.cplex.SetParam(p, unbox v)
+            | :? Cplex.LongParam as p    -> state.cplex.SetParam(p, unbox v)
+            | :? Cplex.StringParam as p  -> state.cplex.SetParam(p, unbox v)
+            | :? Cplex.BooleanParam as p -> state.cplex.SetParam(p, unbox v)
+            | :? Cplex.DoubleParam as p  -> state.cplex.SetParam(p, unbox v)
+            | _ -> failwith $"can't set param value %A{p}"
+          state
         static member removeOption (cplexOptionPredicate: CplexOption -> bool) state = 
             let newOptions =
                 [
@@ -204,9 +357,18 @@ module rec CplexSolver =
                         | :? CplexOption as i ->
                             if not (cplexOptionPredicate i) then
                                 i :> ICPlexOption
+                            else
+                              match i with
+                              | CplexIntOption(o,v) ->    failwith $"solver parameter can't be reset individually {o.GetType().Name}"
+                              | CplexDoubleOption(o,v) -> failwith $"solver parameter can't be reset individually {o.GetType().Name}"
+                              | CplexStringOption(o,v) -> failwith $"solver parameter can't be reset individually {o.GetType().Name}"
+                              | CplexLongOption(o,v) ->   failwith $"solver parameter can't be reset individually {o.GetType().Name}"
+                              | CplexBoolOption(o,v) ->   failwith $"solver parameter can't be reset individually {o.GetType().Name}"
+                              | _ -> ()
                         | _ -> ()
                 ] |> Set.ofList
             { state with options = { state.options with options = newOptions } }
+
 
     type CPlexProblemState = 
         {
