8bf25be203656616eb85b45e315707e163b1bf24
Matthew Crews <matthewcrews@gmail.com>
2020-12-06 22:12:10 -0800

    Adding summer

--- Flips/SliceMap.Extensions.fs
+++ Flips/SliceMap.Extensions.fs
@@ -5,8 +5,17 @@ open SliceMap
 [<AutoOpen>]
 module SliceMap =
   
-  /// <summary>A function which sums the values contained in a SliceMap</summary>
-  /// <param name="x">An instance of ISliceData</param>
-  /// <returns>A LinearExpression with a Unit of Measure</returns>
-  let inline sum (x:ISliceData<'Key, 'Value>) : Flips.Types.LinearExpression =
-      TryFind.sum x.Keys x.TryFind
+    [<AutoOpen>]
+    type Summer () =
+
+        /// A function for summing the contents of a SliceMap
+        static member sum(x:ISliceData<'Key, Flips.Types.Decision>) : Flips.Types.LinearExpression =
+            TryFind.sum x.Keys x.TryFind
+
+        /// A function for summing the contents of a SliceMap
+        static member sum(x:ISliceData<'Key, Flips.Types.LinearExpression>) =
+            TryFind.sum x.Keys x.TryFind
+
+        /// A function for summing the contents of a SliceMap
+        static member sum(x:ISliceData<'Key, float>) : float =
+            TryFind.sum x.Keys x.TryFind
