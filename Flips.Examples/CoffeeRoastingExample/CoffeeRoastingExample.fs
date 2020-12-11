3fa00771dfd9e071ea3fd3ccfab070c4140f9be8
Gauthier Segay <smoothdeveloper@users.noreply.github.com>
2020-12-11 06:32:18 -0800

    move the Flips model definition of all examples into a single SampleProblems.fs (#131)
    * move the Flips model definition of all examples into a single SampleProblems.fs
    
    also define compile constants to allow to reference the different solvers while running the examples.
    
    * some more models running under cplex
    
    some cplex refactoring
    
    * missing files with cplex example code
    
    * move cplex file in Flips.Examples project

--- Flips.Examples/CoffeeRoastingExample/CoffeeRoastingExample.fs
+++ Flips.Examples/CoffeeRoastingExample/CoffeeRoastingExample.fs
@@ -5,114 +5,9 @@ open Flips
 open Flips.Legacy
 open Flips.UnitsOfMeasure
 
-type [<Measure>] USD
-type [<Measure>] ft
-type [<Measure>] Ton
-type [<Measure>] Build
-
-type Location = Location of string
-
 let solve settings =
 
-    let minRoastingCapacity = 30.0<Ton>
-    let minWarehouseCapacity = 30_000.0<ft^3>
-
-    let locations = 
-        [
-            "Sellwood"
-            "Hawthorne"
-            "The Pearl"
-            "Eastmoreland"
-            "St Johns"
-            "Alberta"
-            "Nob Hill"
-            "Belmont"
-        ] |> List.map Location
-
-    let roasterCost = 
-        [
-            Location "Sellwood"      , 150_000.0<USD/Build>
-            Location "Hawthorne"     , 100_000.0<USD/Build> 
-            Location "The Pearl"     , 250_000.0<USD/Build>
-            Location "Eastmoreland"  , 120_000.0<USD/Build>
-            Location "St Johns"      , 130_000.0<USD/Build>
-            Location "Alberta"       , 110_000.0<USD/Build>
-            Location "Nob Hill"      , 135_000.0<USD/Build>
-            Location "Belmont"       , 180_000.0<USD/Build>
-        ] |> SMap.ofList
-
-    let roasterCapacity = 
-        [
-            Location "Sellwood"      , 12.0<Ton/Build>
-            Location "Hawthorne"     , 18.0<Ton/Build> 
-            Location "The Pearl"     , 22.0<Ton/Build>
-            Location "Eastmoreland"  , 13.0<Ton/Build>
-            Location "St Johns"      , 14.0<Ton/Build>
-            Location "Alberta"       , 10.0<Ton/Build>
-            Location "Nob Hill"      , 17.0<Ton/Build>
-            Location "Belmont"       , 12.0<Ton/Build>
-        ] |> SMap.ofList
-
-    let warehouseCost = 
-        [
-            Location "Sellwood"      ,  80_000.0<USD/Build>
-            Location "Hawthorne"     ,  90_000.0<USD/Build> 
-            Location "The Pearl"     , 120_000.0<USD/Build>
-            Location "Eastmoreland"  ,  90_000.0<USD/Build>
-            Location "St Johns"      ,  85_000.0<USD/Build>
-            Location "Alberta"       ,  70_000.0<USD/Build>
-            Location "Nob Hill"      ,  85_000.0<USD/Build>
-            Location "Belmont"       ,  90_000.0<USD/Build>
-        ] |> SMap.ofList
-
-    let warehouseCapacity = 
-        [
-            Location "Sellwood"      ,  8_000.0<ft^3/Build>
-            Location "Hawthorne"     ,  6_000.0<ft^3/Build> 
-            Location "The Pearl"     , 12_000.0<ft^3/Build>
-            Location "Eastmoreland"  ,  6_000.0<ft^3/Build>
-            Location "St Johns"      ,  7_000.0<ft^3/Build>
-            Location "Alberta"       ,  9_000.0<ft^3/Build>
-            Location "Nob Hill"      ,  6_000.0<ft^3/Build>
-            Location "Belmont"       ,  9_200.0<ft^3/Build>
-        ] |> SMap.ofList
-
-    let buildRoaster =
-        DecisionBuilder<Build> "BuildRoaster" {
-            for l in locations ->
-                Boolean
-        } |> SMap.ofSeq
-
-    let buildWarehouse =
-        DecisionBuilder<Build> "BuildWarehouse" {
-            for l in locations ->
-                Boolean
-        } |> SMap.ofSeq
-
-    let minRoastingCapacityConstraint =
-        let lhs = buildRoaster .* roasterCapacity
-        let s = sum lhs
-        Constraint.create "MinRoastingCapacity" (sum (buildRoaster .* roasterCapacity) >== minRoastingCapacity)
-
-    let minWarehouseCapacityConstraint =
-        Constraint.create "MinWarehouseCapacity" (sum (buildWarehouse .* warehouseCapacity) >== minWarehouseCapacity)
-
-    let warehouseWithRoasterConstraints =
-        ConstraintBuilder "WarehouseWithRoaster" {
-            for l in locations ->
-                buildRoaster.[l] <== buildWarehouse.[l]
-        }
-
-    let t = buildRoaster .* roasterCost
-    let objExpr = sum (t) + sum (buildWarehouse .* warehouseCost)
-    let objective = Objective.create "MinimizeCost" Minimize objExpr
-
-    let model = 
-        Model.create objective
-        |> Model.addConstraint minRoastingCapacityConstraint
-        |> Model.addConstraint minWarehouseCapacityConstraint
-        |> Model.addConstraints warehouseWithRoasterConstraints
-
+    let model, objective, buildRoaster, buildWarehouse, locations = SampleProblems.CoffeeRoastingExample.model
     // Call the `solve` function in the Solve module to evaluate the model
     let result = Solver.solve settings model
 
@@ -136,7 +31,7 @@ let solve settings =
         CsvExport.exportVariablesToFile "coffee.roasting" solution.DecisionResults CsvExport.csvConfig
         
         for location in locations do
-            let (Location l) = location
+            let (SampleProblems.CoffeeRoastingExample.Location l) = location
             printfn "%-12s\t%12A\t%12A" l roasterValues.[location] warehouseValues.[location]
     | errorCase -> 
         printfn "Unable to solve. Error: %A" errorCase
--- Flips.Examples/MultipleFoodTrackExample/MultipleFoodTruckExample.fs
+++ Flips.Examples/MultipleFoodTrackExample/MultipleFoodTruckExample.fs
@@ -5,68 +5,7 @@ open Flips.Legacy
 
 
 let solve settings =
-    
-    // Declare the parameters for our model
-    let items = ["Hamburger"; "HotDog"; "Pizza"]
-    let locations = ["Woodstock"; "Sellwood"; "Portland"]
-    let profit = 
-        [
-            (("Woodstock", "Hamburger"), 1.50); (("Sellwood", "Hamburger"), 1.40); (("Portland", "Hamburger"), 1.90)
-            (("Woodstock", "HotDog"   ), 1.20); (("Sellwood", "HotDog"   ), 1.50); (("Portland", "HotDog"   ), 1.80)
-            (("Woodstock", "Pizza"    ), 2.20); (("Sellwood", "Pizza"    ), 1.70); (("Portland", "Pizza"    ), 2.00)
-        ] |> Map.ofList
-
-    let maxIngredients = Map.ofList [("Hamburger", 900.0); ("HotDog", 600.0); ("Pizza", 400.0)]
-    let itemWeight = Map.ofList [("Hamburger", 0.5); ("HotDog", 0.4); ("Pizza", 0.6)]
-    let maxTruckWeight = Map.ofList [("Woodstock", 200.0); ("Sellwood", 300.0); ("Portland", 280.0) ]
-
-    // Create Decision Variable which is keyed by the tuple of Item and Location.
-    // The resulting type is a Map<(string*string),Decision> 
-    // to represent how much of each item we should pack for each location
-    // with a Lower Bound of 0.0 and an Upper Bound of Infinity
-    let numberOfItem =
-        [for item in items do
-            for location in locations do
-                let decName = sprintf "NumberOf_%s_At_%s" item location
-                let decision = Decision.createContinuous decName 0.0 infinity
-                (location, item), decision]
-        |> Map.ofList
-
-    // Create the Linear Expression for the objective
-    let objectiveExpression = 
-        [for item in items do
-            for location in locations ->
-                profit.[location, item] * numberOfItem.[location, item]]
-        |> List.sum            
-
-    // Create an Objective with the name "MaximizeRevenue" the goal of Maximizing
-    // the Objective Expression
-    let objective = Objective.create "MaximizeRevenue" Maximize objectiveExpression
-    
-    // Create Total Item Maximum constraints for each item
-    let maxItemConstraints =
-        [for item in items do
-            // The total of the Item is the sum across the Locations
-            let locationSum = List.sum [for location in locations -> 1.0 * numberOfItem.[location, item]]
-            let name = sprintf "MaxItemTotal_%s" item
-            Constraint.create name (locationSum <== maxIngredients.[item])
-        ]
-
-
-    // Create a Constraint for the Max combined weight of items for each Location
-    let maxWeightConstraints = 
-        [for location in locations -> 
-            let weightSum = List.sum [for item in items -> itemWeight.[item] * numberOfItem.[location, item]]
-            let name = sprintf "MaxTotalWeight_%s" location
-            Constraint.create name (weightSum <== maxTruckWeight.[location])
-        ]
-
-    // Create a Model type and pipe it through the addition of the constraints
-    let model =
-        Model.create objective
-        |> Model.addConstraints maxItemConstraints
-        |> Model.addConstraints maxWeightConstraints
-
+    let model, objective = SampleProblems.MultipleFoodTruckExample.model
     // Call the `solve` function in the Solve module to evaluate the model
     let result = Solver.solve settings model
 
