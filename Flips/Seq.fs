9c7ae7bce836693a42b5f23a9df4498abc6ce1be
Asti <asti@dynz.net>
2021-01-11 12:08:00 +0530

    Use built-in SequenceEquals (#164)
    Reduce surface required to test

--- Flips/Seq.fs
+++ Flips/Seq.fs
@@ -3,8 +3,5 @@
 module Seq =
 
     let equals a b =
-        let result = 
-          Seq.zip a b 
-          |> Seq.forall (fun (x, y) -> x = y)
-        result
+        System.Linq.Enumerable.SequenceEqual(a, b)
 
